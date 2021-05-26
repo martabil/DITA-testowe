c134e336f44d4886a8274fa3630542f1f7924b6d
martabil <77537664+martabil@users.noreply.github.com>
2021-05-26 21:04:23 +0200

    testowe upload

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,16 @@
+define(function() {var keywords=[{w:"My",p:["p0","p1","p2","p3","p4","p5"]},{w:"first",p:["p0","p3","p4"]},{w:"concept",p:["p0","p1","p2"]},{w:"second",p:["p1","p5"]},{w:"third",p:["p2"]},{w:"glossentry",p:["p3"]},{w:"reference",p:["p4","p5"]},{w:"topic",p:["p4","p5"]},{w:"Showing",p:["p6"]},{w:"full",p:["p6"]},{w:"tags",p:["p6"]},{w:"in",p:["p6"]},{w:"the",p:["p6"]},{w:"Oxygen",p:["p6"]},{w:"author",p:["p6"]},{w:"view",p:["p6","p7"]},{w:"by",p:["p6"]},{w:"default",p:["p6"]},{w:"Setting",p:["p7"]},{w:"up",p:["p7"]},{w:"text",p:["p7"]},{w:"mode",p:["p7"]},{w:"part",p:["p7"]},{w:"1",p:["p7"]},{w:"Generating",p:["p8"]},{w:"HTML",p:["p8"]},{w:"site",p:["p8"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[0, 3, 2];
+ph["p2"]=[0, 4, 2];
+ph["p3"]=[0, 1, 5];
+ph["p4"]=[0, 1, 6, 7];
+ph["p5"]=[0, 3, 6, 7];
+ph["p6"]=[8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+ph["p7"]=[18, 19, 20, 21, 15, 22, 23];
+ph["p8"]=[24, 25, 26];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
